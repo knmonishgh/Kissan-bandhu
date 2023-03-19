16bf433948ac8b78e52d021749b97fb3338b497d
BCKeerthi <bckeerthi318@gmail.com>
2023-03-19 20:08:26 +0530

    Updated buyer home page

--- app/src/main/java/com/example/kissanbandhu/buyerlogin.java
+++ app/src/main/java/com/example/kissanbandhu/buyerlogin.java
@@ -1,14 +1,128 @@
 package com.example.kissanbandhu;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class buyerlogin extends AppCompatActivity {
 
+    EditText phone, otp;
+    Button btngen, btnverify;
+    FirebaseAuth mAuth;
+    String verificationid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buyerlogin);
+
+        phone = findViewById(R.id.phone);
+        otp = findViewById(R.id.otp);
+        btngen = findViewById(R.id.genotp_button);
+        btnverify = findViewById(R.id.verifyotp_button);
+        mAuth = FirebaseAuth.getInstance();
+
+        btngen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(phone.getText().toString())){
+                    Toast.makeText(buyerlogin.this, "Enter valid phone number", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    String num = phone.getText().toString();
+                    sendverificationcode(num);
+                }
+            }
+        });
+
+        btnverify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(otp.getText().toString())){
+                    Toast.makeText(buyerlogin.this, "Please enter OTP", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    verifycode(otp.getText().toString());
+                }
+            }
+        });
+
+    }
+
+    private void sendverificationcode(String phoneNumber) {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber("+91 "+phoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+            mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential credential) {
+            final String code = credential.getSmsCode();
+            if(code!=null){
+                verifycode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+            Toast.makeText(buyerlogin.this, "Wrong Number", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCodeSent(@NonNull String s,
+                               @NonNull PhoneAuthProvider.ForceResendingToken token) {
+            super.onCodeSent(s, token);
+            verificationid = s;
+            Toast.makeText(buyerlogin.this, "OTP Sent", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    //till here number verification
+    private void verifycode(String Code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationid, Code);
+        signinbycredentials(credential);
+    }
+
+    private void signinbycredentials(PhoneAuthCredential credential) {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(buyerlogin.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(buyerlogin.this, buyerhomepage.class));
+                        }
+                        else{
+                            Toast.makeText(buyerlogin.this, "Wrong OTP", Toast.LENGTH_SHORT).show();
+                            otp.setText("");
+                        }
+                    }
+                });
     }
 }
