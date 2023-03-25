ab022a7b490d980bf8fa7b320a575fbcc9f1b681
K N Monish Kumar <monishkumar810@gmail.com>
2023-03-25 23:29:29 +0530

    updated product menu , added image

---
+++ app/src/main/java/com/example/kissanbandhu/rentcart.java
@@ -0,0 +1,18 @@
+package com.example.kissanbandhu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class rentcart extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rentcart);
+
+    }
+
+}
