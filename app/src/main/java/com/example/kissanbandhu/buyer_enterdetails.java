fc7a69774ad2ddd4ebc5a49e7f8b84e1664cc816
BCKeerthi <bckeerthi318@gmail.com>
2023-03-29 22:06:15 +0530

    Updated nav in all pages

--- app/src/main/java/com/example/kissanbandhu/buyer_enterdetails.java
+++ app/src/main/java/com/example/kissanbandhu/buyer_enterdetails.java
@@ -3,6 +3,7 @@ package com.example.kissanbandhu;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -12,14 +13,15 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class orders extends AppCompatActivity {
+public class buyer_enterdetails extends AppCompatActivity {
+
     BottomNavigationView nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_orders);
-        getSupportActionBar().setTitle("ORDERS");
+        setContentView(R.layout.activity_buyer_enterdetails);
+        getSupportActionBar().setTitle("BUYER");
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#547AE1")));
         nav = findViewById(R.id.nav);
         nav.setOnItemReselectedListener(new NavigationBarView.OnItemReselectedListener() {
@@ -28,13 +30,13 @@ public class orders extends AppCompatActivity {
                 switch (item.getItemId()){
 
                     case R.id.homenav:
-                        startActivity(new Intent(orders.this,buyerhomepage.class));
+                        startActivity(new Intent(buyer_enterdetails.this,buyerhomepage.class));
                         break;
                     case R.id.cartnav:
-                        startActivity(new Intent(orders.this,rentcart.class));
+                        startActivity(new Intent(buyer_enterdetails.this,rentcart.class));
                         break;
                     case R.id.ordersnav:
-                        startActivity(new Intent(orders.this,orders.class));
+                        startActivity(new Intent(buyer_enterdetails.this,orders.class));
                         break;
                 }
             }
--- app/src/main/java/com/example/kissanbandhu/rentcart.java
+++ app/src/main/java/com/example/kissanbandhu/rentcart.java
@@ -1,18 +1,45 @@
 package com.example.kissanbandhu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class rentcart extends AppCompatActivity {
 
-
+    BottomNavigationView nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rentcart);
+        getSupportActionBar().setTitle("CART");
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#547AE1")));
+        nav = findViewById(R.id.nav);
+        nav.setOnItemReselectedListener(new NavigationBarView.OnItemReselectedListener(){
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
 
+                    case R.id.homenav:
+                        startActivity(new Intent(rentcart.this,buyerhomepage.class));
+                        break;
+                    case R.id.cartnav:
+                        startActivity(new Intent(rentcart.this,rentcart.class));
+                        break;
+                    case R.id.ordersnav:
+                        startActivity(new Intent(rentcart.this,orders.class));
+                        break;
+                }
+            }
+        });
     }
 
 }
